5dee51a219c149bf48ab25dea920db7affbbad52
jeyhun mammadsaidov <jeyhun.mammadsaidov@goldenpay.az>
2026-02-08 23:59:44 +0400

    initial commit

---
+++ src/main/java/az/gaming_cafe/gaming_cafe/GamingCafeApplication.java
@@ -0,0 +1,13 @@
+package az.gaming_cafe.gaming_cafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamingCafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamingCafeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/az/gaming_cafe/gaming_cafe/GamingCafeApplicationTests.java
@@ -0,0 +1,13 @@
+package az.gaming_cafe.gaming_cafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamingCafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
