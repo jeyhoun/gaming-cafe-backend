fba109e2bfd4e32f0c91e606d59bb63d2292d8c3
jeyhun mammadsaidov <jeyhun.mammadsaidov@goldenpay.az>
2026-02-15 11:58:00 +0400

    GCD-26: removed unimportant annotation for map

--- src/main/java/az/gaming_cafe/mapper/ComputerMapper.java
+++ src/main/java/az/gaming_cafe/mapper/ComputerMapper.java
@@ -3,12 +3,10 @@ package az.gaming_cafe.mapper;
 import az.gaming_cafe.model.dto.request.ComputerRequestDto;
 import az.gaming_cafe.model.dto.response.ComputerResponseDto;
 import az.gaming_cafe.model.entity.ComputerEntity;
-import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.Mappings;
-import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.mapstruct.ReportingPolicy;
 
 import java.util.List;
@@ -36,6 +34,5 @@ public interface ComputerMapper {
             @Mapping(target = "updatedBy", ignore = true),
             @Mapping(target = "id", ignore = true)
     })
-    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
     void updateComputerFromDto(ComputerRequestDto computerRequestDto, @MappingTarget ComputerEntity computerEntity);
 }
